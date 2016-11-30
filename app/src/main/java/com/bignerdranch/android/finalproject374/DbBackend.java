e449680989581a4e6b16d0294ab1cfd234405b3f
wrenywren <wren.chance@gmail.com>
2016-11-29 19:57:43 -0500

    Added necessary classes to interact with databases. Also added assets/databases file path to store database files

---
+++ app/src/main/java/com/bignerdranch/android/finalproject374/DbBackend.java
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.finalproject374;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by Wren on 10/23/2016.
+ */
+
+public class DbBackend extends DbObject {
+    public DbBackend(Context context) {
+        super(context);
+    }
+
+
+
+
+}
